e10c94c16270883e41d4d06c3b66c4f065db537e
Max Kruse <maximilian.kruse@partner.kit.edu>
2025-10-23 10:08:15 +0200

    Add prior and fiber field exploration

---
+++ src/cardiac_electrophysiology/fiberfield.py
@@ -0,0 +1,72 @@
+from typing import final
+
+import jax
+import jax.numpy as jnp
+import numpy.typing as npt
+from eikonax import tensorfield
+from jaxtyping import Int as jtInt
+from jaxtyping import Real as jtReal
+
+
+# ==================================================================================================
+@final
+class FiberTensor(tensorfield.AbstractSimplexTensor):
+    _mean_parameter_vector: jtReal[jax.Array, "num_parameters"]
+    _mean_angle_vector: jtReal[jax.Array, "num_parameters"]
+    _basis_vectors: tuple[
+        jtReal[jax.Array, "num_parameters dim"],
+        jtReal[jax.Array, "num_parameters dim"],
+    ]
+    _conduction_velocities: tuple[
+        jtReal[jax.Array, "num_parameters"],
+        jtReal[jax.Array, "num_parameters"],
+    ]
+
+    # ----------------------------------------------------------------------------------------------
+    def __init__(
+        self,
+        dimension: int,
+        mean_parameter_vector: jtReal[jax.Array | npt.NDArray, "num_parameters"],
+        basis_vectors: tuple[
+            jtReal[jax.Array | npt.NDArray, "num_parameters dim"],
+            jtReal[jax.Array | npt.NDArray, "num_parameters dim"],
+        ],
+        conduction_velocities: tuple[
+            jtReal[jax.Array | npt.NDArray, "num_parameters"],
+            jtReal[jax.Array | npt.NDArray, "num_parameters"],
+        ],
+    ) -> None:
+        self.dimension = dimension
+        self._mean_parameter_vector = mean_parameter_vector
+        self._mean_angle_vector = jnp.arccos(jnp.tanh(mean_parameter_vector))
+        self._basis_vectors = basis_vectors
+        self._conduction_velocities = conduction_velocities
+
+    # ----------------------------------------------------------------------------------------------
+    def assemble(
+        self,
+        simplex_ind: jtInt[jax.Array, ""],
+        parameter: jtReal[jax.Array, "num_parameters_local"],
+    ) -> jtReal[jax.Array, "dim dim"]:
+        e_1 = self._basis_vectors[0][simplex_ind]
+        e_2 = self._basis_vectors[1][simplex_ind]
+        long_velocity = self._conduction_velocities[0][simplex_ind]
+        trans_velocity = self._conduction_velocities[1][simplex_ind]
+        mean_angle = self._mean_angle_vector[simplex_ind]
+        angle = jnp.arccos(jnp.tanh(parameter)) + mean_angle - jnp.pi / 2
+        long_vector =  jnp.cos(angle) * e_1 + jnp.sin(angle) * e_2
+        trans_vector = -jnp.sin(angle) * e_1 + jnp.cos(angle) * e_2
+        tensor = 1 / jnp.square(long_velocity) * jnp.outer(
+            long_vector, long_vector
+        ) + 1 / jnp.square(trans_velocity) * jnp.outer(trans_vector, trans_vector)
+        return tensor
+
+    # ----------------------------------------------------------------------------------------------
+    def derivative(
+        self,
+        simplex_ind: jtInt[jax.Array, ""],
+        parameter: jtReal[jax.Array, "num_parameters_local"],
+    ) -> jtReal[jax.Array, "dim dim num_local_parameters"]:
+        derivative_function = jax.jacfwd(self.assemble, argnums=1)
+        tensor = derivative_function(simplex_ind, parameter)
+        return tensor
