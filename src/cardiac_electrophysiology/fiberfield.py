cb551f2ed413fa76bd8bad6614ca4ef7564aff68
Max Kruse <maxkruse1604@gmail.com>
2025-10-24 00:19:44 +0200

    feat: Enable cuda installation for jax

--- src/cardiac_electrophysiology/fiberfield.py
+++ src/cardiac_electrophysiology/fiberfield.py
@@ -2,7 +2,10 @@ from typing import final
 
 import jax
 import jax.numpy as jnp
+import numpy as np
 import numpy.typing as npt
+import pyvista as pv
+import scipy.sparse as sp
 from eikonax import tensorfield
 from jaxtyping import Int as jtInt
 from jaxtyping import Real as jtReal
@@ -54,7 +57,7 @@ class FiberTensor(tensorfield.AbstractSimplexTensor):
         trans_velocity = self._conduction_velocities[1][simplex_ind]
         mean_angle = self._mean_angle_vector[simplex_ind]
         angle = jnp.arccos(jnp.tanh(parameter)) + mean_angle - jnp.pi / 2
-        long_vector =  jnp.cos(angle) * e_1 + jnp.sin(angle) * e_2
+        long_vector = jnp.cos(angle) * e_1 + jnp.sin(angle) * e_2
         trans_vector = -jnp.sin(angle) * e_1 + jnp.cos(angle) * e_2
         tensor = 1 / jnp.square(long_velocity) * jnp.outer(
             long_vector, long_vector
@@ -70,3 +73,18 @@ class FiberTensor(tensorfield.AbstractSimplexTensor):
         derivative_function = jax.jacfwd(self.assemble, argnums=1)
         tensor = derivative_function(simplex_ind, parameter)
         return tensor
+
+
+# ==================================================================================================
+def assemble_vertex_to_simplex_interpolation_matrix(mesh: pv.PolyData) -> sp.coo_array:
+    num_vertices = mesh.number_of_points
+    num_simplices = mesh.number_of_cells
+
+    simplices = np.array(mesh.faces.reshape(-1, 4)[:, 1:])
+    row_inds = np.repeat(np.arange(num_simplices), 3)
+    col_inds = simplices.flatten()
+    data = np.full(num_simplices * 3, 1 / 3)
+    interpolation_matrix = sp.coo_array(
+        (data, (row_inds, col_inds)), shape=(num_simplices, num_vertices)
+    )
+    return interpolation_matrix
