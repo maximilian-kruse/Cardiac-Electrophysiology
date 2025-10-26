bebbe3555d654519c9a19393be011dc25d54b0b6
Max Kruse <maximilian.kruse@partner.kit.edu>
2025-10-26 17:56:34 +0100

    feat:implement_forward_uq_test

---
+++ src/cardiac_electrophysiology/data_processing.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pyvista as pv
+import scipy.sparse as sp
+import scipy.stats as st
+
+
+# ==================================================================================================
+def compute_axial_mean_and_variance(
+    angle_samples: np.ndarray[tuple[int], np.dtype[np.float64]], axis: int = 0
+) -> tuple[
+    np.ndarray[tuple[int], np.dtype[np.float64]], np.ndarray[tuple[int], np.dtype[np.float64]]
+]:
+    normalized_angle_samples = np.mod(angle_samples, np.pi)
+    circ_mean_doubled = st.circmean(2 * normalized_angle_samples, axis=axis)
+    circ_std_doubled = st.circstd(2 * normalized_angle_samples, axis=axis)
+    axial_mean = circ_mean_doubled / 2
+    axial_variance = (circ_std_doubled / 2) ** 2
+
+    shift_by_pi_mask = (axial_mean > 1 / 2 * np.pi) & (axial_mean <= 3 / 2 * np.pi)
+    shift_by_two_pi_mask = axial_mean > 3 / 2 * np.pi
+    axial_mean[shift_by_pi_mask] -= np.pi
+    axial_mean[shift_by_two_pi_mask] -= 2 * np.pi
+
+    return axial_mean, axial_variance
+
+
+# ==================================================================================================
+def assemble_vertex_to_simplex_interpolation_matrix(
+    connectivity: np.ndarray[tuple[int, int], np.dtype[np.float64]],
+) -> sp.coo_array:
+    num_vertices = np.max(connectivity) + 1
+    num_simplices = connectivity.shape[0]
+    row_inds = np.repeat(np.arange(num_simplices), 3)
+    col_inds = connectivity.flatten()
+    data = np.full(num_simplices * 3, 1 / 3)
+    interpolation_matrix = sp.coo_array(
+        (data, (row_inds, col_inds)), shape=(num_simplices, num_vertices)
+    )
+    return interpolation_matrix
+
+
+# --------------------------------------------------------------------------------------------------
+def assemble_simplex_to_vertex_interpolation_matrix(
+    connectivity: np.ndarray[tuple[int, int], np.dtype[np.float64]],
+) -> sp.coo_array:
+    vertex_to_simplex_matrix = assemble_vertex_to_simplex_interpolation_matrix(connectivity)
+    num_adjacent_cells = vertex_to_simplex_matrix.sum(axis=0)
+    normalization_matrix = sp.diags_array(1 / num_adjacent_cells).tocoo()
+    simplex_to_vertex_matrix = normalization_matrix @ vertex_to_simplex_matrix.transpose()
+    return simplex_to_vertex_matrix
--- src/cardiac_electrophysiology/fiberfield.py
+++ src/cardiac_electrophysiology/fiberfield.py
@@ -2,10 +2,7 @@ from typing import final
 
 import jax
 import jax.numpy as jnp
-import numpy as np
 import numpy.typing as npt
-import pyvista as pv
-import scipy.sparse as sp
 from eikonax import tensorfield
 from jaxtyping import Int as jtInt
 from jaxtyping import Real as jtReal
@@ -16,20 +13,16 @@ from jaxtyping import Real as jtReal
 class FiberTensor(tensorfield.AbstractSimplexTensor):
     _mean_parameter_vector: jtReal[jax.Array, "num_parameters"]
     _mean_angle_vector: jtReal[jax.Array, "num_parameters"]
-    _basis_vectors: tuple[
-        jtReal[jax.Array, "num_parameters dim"],
-        jtReal[jax.Array, "num_parameters dim"],
-    ]
-    _conduction_velocities: tuple[
-        jtReal[jax.Array, "num_parameters"],
-        jtReal[jax.Array, "num_parameters"],
-    ]
+    _first_basis_vectors: jtReal[jax.Array, "num_parameters dim"]
+    _second_basis_vectors: jtReal[jax.Array, "num_parameters dim"]
+    _longitudinal_velocities: jtReal[jax.Array, "num_parameters"]
+    _transversal_velocities: jtReal[jax.Array, "num_parameters"]
 
     # ----------------------------------------------------------------------------------------------
     def __init__(
         self,
         dimension: int,
-        mean_parameter_vector: jtReal[jax.Array | npt.NDArray, "num_parameters"],
+        mean_angle_vector: jtReal[jax.Array | npt.NDArray, "num_parameters"],
         basis_vectors: tuple[
             jtReal[jax.Array | npt.NDArray, "num_parameters dim"],
             jtReal[jax.Array | npt.NDArray, "num_parameters dim"],
@@ -40,10 +33,12 @@ class FiberTensor(tensorfield.AbstractSimplexTensor):
         ],
     ) -> None:
         self.dimension = dimension
-        self._mean_parameter_vector = mean_parameter_vector
-        self._mean_angle_vector = jnp.arccos(jnp.tanh(mean_parameter_vector))
-        self._basis_vectors = basis_vectors
-        self._conduction_velocities = conduction_velocities
+        self._mean_angle_vector = jnp.array(mean_angle_vector, dtype=jnp.float32)
+        self._mean_parameter_vector = jnp.arctanh(jnp.cos(self._mean_angle_vector + jnp.pi / 2))
+        self._first_basis_vectors = jnp.array(basis_vectors[0], dtype=jnp.float32)
+        self._second_basis_vectors = jnp.array(basis_vectors[1], dtype=jnp.float32)
+        self._longitudinal_velocities = jnp.array(conduction_velocities[0], dtype=jnp.float32)
+        self._transversal_velocities = jnp.array(conduction_velocities[1], dtype=jnp.float32)
 
     # ----------------------------------------------------------------------------------------------
     def assemble(
@@ -51,12 +46,13 @@ class FiberTensor(tensorfield.AbstractSimplexTensor):
         simplex_ind: jtInt[jax.Array, ""],
         parameter: jtReal[jax.Array, "num_parameters_local"],
     ) -> jtReal[jax.Array, "dim dim"]:
-        e_1 = self._basis_vectors[0][simplex_ind]
-        e_2 = self._basis_vectors[1][simplex_ind]
-        long_velocity = self._conduction_velocities[0][simplex_ind]
-        trans_velocity = self._conduction_velocities[1][simplex_ind]
+        e_1 = self._first_basis_vectors[simplex_ind]
+        e_2 = self._second_basis_vectors[simplex_ind]
+        long_velocity = self._longitudinal_velocities[simplex_ind]
+        trans_velocity = self._transversal_velocities[simplex_ind]
         mean_angle = self._mean_angle_vector[simplex_ind]
-        angle = jnp.arccos(jnp.tanh(parameter)) + mean_angle - jnp.pi / 2
+        centered_parameter = parameter - self._mean_parameter_vector[simplex_ind]
+        angle = jnp.arccos(jnp.tanh(centered_parameter)) + mean_angle - jnp.pi / 2
         long_vector = jnp.cos(angle) * e_1 + jnp.sin(angle) * e_2
         trans_vector = -jnp.sin(angle) * e_1 + jnp.cos(angle) * e_2
         tensor = 1 / jnp.square(long_velocity) * jnp.outer(
@@ -73,18 +69,3 @@ class FiberTensor(tensorfield.AbstractSimplexTensor):
         derivative_function = jax.jacfwd(self.assemble, argnums=1)
         tensor = derivative_function(simplex_ind, parameter)
         return tensor
-
-
-# ==================================================================================================
-def assemble_vertex_to_simplex_interpolation_matrix(mesh: pv.PolyData) -> sp.coo_array:
-    num_vertices = mesh.number_of_points
-    num_simplices = mesh.number_of_cells
-
-    simplices = np.array(mesh.faces.reshape(-1, 4)[:, 1:])
-    row_inds = np.repeat(np.arange(num_simplices), 3)
-    col_inds = simplices.flatten()
-    data = np.full(num_simplices * 3, 1 / 3)
-    interpolation_matrix = sp.coo_array(
-        (data, (row_inds, col_inds)), shape=(num_simplices, num_vertices)
-    )
-    return interpolation_matrix
